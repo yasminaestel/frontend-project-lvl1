c488faf3617b94207a27f40f89e660da122adeb2
Svetlana Khodak <yasminaestel@gmail.com>
2022-02-16 16:48:57 +0200

    step 4.2

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,7 @@
+
+
+/* eslint-disable no-console */
+
 import readlineSync from "readline-sync";
 console.log('Welcome to the Brain Games!');
 export const userName = readlineSync.question("May I have your name? ");
