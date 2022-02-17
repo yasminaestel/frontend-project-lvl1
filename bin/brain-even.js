27e4154aab2b5c25f5b740075f5b5c0635918278
Svetlana Khodak <yasminaestel@gmail.com>
2022-02-17 20:34:15 +0200

    new fix

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 
 import {username} from "../src/games/index-even.js";
-import {youAnswer} from "../src/games/index-even.js";
+import gameEven from "../src/games/index-even.js";
