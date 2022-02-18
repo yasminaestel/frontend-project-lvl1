9a393e925a71ab67a102723a45c6b181984a0a8f
yasminaestel <98806463+yasminaestel@users.noreply.github.com>
2022-02-18 15:07:24 +0200

    Create getRandomOperation.js

---
+++ src/getRandomOperation.js
@@ -0,0 +1,11 @@
+const getRandomOperator = () => {
+  const randomPercentage = Math.random();
+  if (randomPercentage < 0.33) {
+    return '+';
+  } else if (randomPercentage < 0.66) {
+    return '-';
+  } else {
+    return '*';
+  }
+};
+export defaukt getRandomOperator;
