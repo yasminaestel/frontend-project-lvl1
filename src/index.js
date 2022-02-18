70ee6c8a806126e11047a9f079ad23c38a7e5dce
yasminaestel <98806463+yasminaestel@users.noreply.github.com>
2022-02-18 08:26:54 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import readlineSync from "readline-sync";
 export const startGames = () => {
   console.log("Welcome to the Brain Games!");
   const username = readlineSync.question("May I have your name? ");
