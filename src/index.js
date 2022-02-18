c6890ed851122d720e2a2e688d642c3340c8014c
yasminaestel <98806463+yasminaestel@users.noreply.github.com>
2022-02-18 10:28:27 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import readlineSync from "readline-sync";
-export const username = readlineSync.question("May I have your name? ");
+export const username = () => readlineSync.question("May I have your name? ");
 export const startGames = () => {
   console.log("Welcome to the Brain Games!");
- const myName = username;
+ username ();
   console.log(`Hello, ${myName}!`);}
 
