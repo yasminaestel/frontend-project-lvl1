6b5066399187603221957eb8ad5e542aa992a0fa
yasminaestel <98806463+yasminaestel@users.noreply.github.com>
2022-02-18 11:00:38 +0200

    Create index-calc.js

---
+++ src/games/index-calc.js
@@ -0,0 +1,28 @@
+import getRandomNumber from "../getRandomNumber.js";
+import readlineSync from "readline-sync";
+
+
+  console.log("Welcome to the Brain Games!");
+  const username = readlineSync.question("May I have your name? ");
+  console.log(`Hello, ${username}!`);
+  console.log('What is the result of the expression?');
+  const gameCalc = () => {
+    let attemptCounter = 0;
+    for (let i = 1; i <= 3; i += 1) {
+    const number = `${getRandomNumber()} + ${getRandomNumber()}`;
+    const numberResult = getRandomNumber() + getRandomNumber();
+    console.log("Question:" + number);
+    const youAnswer = readlineSync.question("You answer: ");
+    if (numberResult === youAnswer) {
+      console.log("Correct!");
+      attemptCounter += 1;
+    } else {
+      console.log(`'${youAnswer}' is wrong answer ;(. Correct answer was '${numberResult}'.`);
+      console.log(`Let's try again, ${username}!`);
+      return;
+    }
+  };
+  if (attemptCounter === 3) {
+    console.log(`Congratulations, ${ username }!`);
+    }
+  }
