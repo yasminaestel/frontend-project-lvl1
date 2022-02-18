f5263ee25d4e7d2105a5a9ab4992c167977960d0
yasminaestel <98806463+yasminaestel@users.noreply.github.com>
2022-02-18 08:15:29 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const startGames = () => {
+  console.log("Welcome to the Brain Games!");
+export const username = readlineSync.question("May I have your name? ");
+console.log(`Hello, ${username}!`);}
+
+export defalt startGames;
