8d92a0597e0a0c3c5e563c7393e866b12ee8db3e
Svetlana Khodak <yasminaestel@gmail.com>
2022-02-18 10:47:09 +0200

    18.2 start

--- src/games/index-even.js
+++ src/games/index-even.js
@@ -4,7 +4,7 @@ import readlineSync from "readline-sync";
 
   console.log("Welcome to the Brain Games!");
   const username = readlineSync.question("May I have your name? ");
-  console.log(`Hello, ${username}!`);}
+  console.log(`Hello, ${username}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
   const gameEven = () => {
   let attemptCounter = 0;
