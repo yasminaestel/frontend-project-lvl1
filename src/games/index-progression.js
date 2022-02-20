6d5b79095415eade932aa33874ea8fb9e4fcc680
yasminaestel <98806463+yasminaestel@users.noreply.github.com>
2022-02-20 16:21:23 +0200

    Update index-progression.js

--- src/games/index-progression.js
+++ src/games/index-progression.js
@@ -9,34 +9,35 @@ const username = readlineSync.question("May I have your name? ");
 console.log(`Hello, ${username}!`);
 console.log("What number is missing in the progression?");
 const gameProgression = () => {
-    let attemptCounter = 0;
-    for (let i = 1; i <= 3; i += 1) {
-        let firstIndex = getRandomNumber();
-        let difference = randomDiff();
-        let arrayLength = randomLength();
-        let massiveRandom = [];
-        massiveRandom = massiveRandom.push(firstIndex);
-        for (let index = 1; index <= arrayLength; index++) {
-            massiveRandom = massiveRandom.push(massiveRandom[index - 1] + difference);
-        }
-        let number = randomDiff();
-        let answerRight = massiveRandom[number];
-        let a = massiveRandom.splice(number, 1, "..");
-        console.log(`Question: ${massiveRandom.join()}`);
-        const youAnswer = readlineSync.question("You answer: ");
-        if (youAnswer === `${answerRight}`) {
-            console.log("Correct!");
-            attemptCounter += 1;
-        } else {
-            console.log(
-                `'${youAnswer}' is wrong answer ;(. Correct answer was ${answerRight}.`
-            );
-            console.log(`Let's try again, ${username}!`);
-            return;
-        }
+  let attemptCounter = 0;
+  for (let i = 1; i <= 3; i += 1) {
+    let firstIndex = getRandomNumber();
+    let difference = randomDiff();
+    let arrayLength = randomLength();
+    let massiveRandom = [];
+    massiveRandom = massiveRandom.push(firstIndex);
+    for (let index = 1; index <= arrayLength; index++) {
+      number2 = massiveRandom[index - 1] + difference;
+        let q = massiveRandom.push(number2);
     }
-    if (attemptCounter === 3) {
-        console.log(`Congratulations, ${username}!`);
+    let number = randomDiff();
+    let answerRight = massiveRandom[number];
+    let a = massiveRandom.splice(number, 1, "..");
+    console.log(`Question: ${massiveRandom.join()}`);
+    const youAnswer = readlineSync.question("You answer: ");
+    if (youAnswer === `${answerRight}`) {
+      console.log("Correct!");
+      attemptCounter += 1;
+    } else {
+      console.log(
+        `'${youAnswer}' is wrong answer ;(. Correct answer was ${answerRight}.`
+      );
+      console.log(`Let's try again, ${username}!`);
+      return;
     }
+  }
+  if (attemptCounter === 3) {
+    console.log(`Congratulations, ${username}!`);
+  }
 };
 export default gameProgression;
