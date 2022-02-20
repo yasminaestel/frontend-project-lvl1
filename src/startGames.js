562978eeb5748691e4090f7aeaed0353eb0f2e5f
yasminaestel <98806463+yasminaestel@users.noreply.github.com>
2022-02-20 08:14:03 +0200

    Update startGames.js

--- src/startGames.js
+++ src/startGames.js
@@ -1,6 +1,6 @@
 const startGames = () => {
 console.log("Welcome to the Brain Games!");
-  const username = readlineSync.question("May I have your name? ");
+  export const username = readlineSync.question("May I have your name? ");
   console.log(`Hello, ${username}!`);
   console.log('What is the result of the expression?');
 }
