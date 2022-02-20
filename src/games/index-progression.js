bb58cbcf2e947495b1107c7362cfcd100f69a908
Svetlana Khodak <yasminaestel@gmail.com>
2022-02-20 18:43:11 +0200

    add code in index-progression

--- src/games/index-progression.js
+++ src/games/index-progression.js
@@ -18,9 +18,8 @@ const gameProgression = () => {
     lineMassive = lineMassive.push(firstIndex)
     for (let index = 1; index < arrayLength; index += 1) {
     let intermediateVariable = lineMassive[index-1] + difference;
-    let q = lineMassive.push(intermediateVariable);
-      lineMassive = q;
-    }
+	lineMassive.push(intermediateVariable);
+         }
     let number = difference;
     let answerRight = lineMassive[number];
     let lineQuestion = lineMassive.splice(number, 1, "..");
