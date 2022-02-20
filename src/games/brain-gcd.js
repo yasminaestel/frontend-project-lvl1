e53e8f6636cd9da8c7e572167895b691f6d135ff
Svetlana Khodak <yasminaestel@gmail.com>
2022-02-20 09:05:29 +0200

    add brain-gcs.js

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -6,8 +6,39 @@ startGames ();
 const username = readlineSync.question("May I have your name? ");
 console.log(`Hello, ${username}!`);
 console.log('Find the greatest common divisor of given numbers.');
-const gameGcd = () => {
-const a = getRandomNumber();
-const b = getRandomNumber();
-const number = `${a} ${b}`;
-console.log('Question: a b' );
+let attemptCounter = 0;
+    for (let i = 1; i <= 3; i += 1) {
+        const number1 = getRandomNumber();
+        const number2 = getRandomNumber();
+        console.log(`Question: ${number1} ${number2}` );
+        const youAnswer = readlineSync.question("You answer: ");
+        const gameGcd = () => {
+            const nod (number1, number2) => {
+                let number3;
+                while (number1 != number2) {
+                    if (number1 > number2) {
+                        number1 = number1 - number2;
+
+                    } else
+                    if (number1 < number2) {
+                        number3 = number1;
+                        number1 = number2;
+                        number2 = number3;
+                    }
+
+                }
+                return number1;
+            }
+        }
+    if (youAnswer === `${number1}`) {
+        console.log("Correct!");
+        attemptCounter += 1;
+    } else {
+        console.log(`'${youAnswer}' is wrong answer ;(. Correct answer was ${numberAnswer}.`);
+        console.log(`Let's try again, ${username}!`);
+        return;
+    }
+}
+if (attemptCounter === 3) {
+    console.log(`Congratulations, ${ username }!`);
+    }
