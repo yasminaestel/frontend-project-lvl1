14c07c462ca71c3f951848120671a5eda43dae78
yasminaestel <98806463+yasminaestel@users.noreply.github.com>
2022-02-20 11:29:16 +0200

    Update index-gcd.js

--- src/games/index-gcd.js
+++ src/games/index-gcd.js
@@ -2,43 +2,40 @@ import getRandomNumber from "../getRandomNumber.js";
 import readlineSync from "readline-sync";
 import startGames from "../startGames.js";
 
-startGames ();
+startGames();
 const username = readlineSync.question("May I have your name? ");
 console.log(`Hello, ${username}!`);
-console.log('Find the greatest common divisor of given numbers.');
+console.log("Find the greatest common divisor of given numbers.");
 const gameGcd = () => {
-let attemptCounter = 0;
-    for (let i = 1; i <= 3; i += 1) {
-        const number1 = getRandomNumber();
-        const number2 = getRandomNumber();
-        console.log(`Question: ${number1} ${number2}` );
-        const youAnswer = readlineSync.question("You answer: ");
-        const nod = (number1, number2) => {
-   let gcd;
+  let attemptCounter = 0;
+  for (let i = 1; i <= 3; i += 1) {
+    let number1 = getRandomNumber();
+    let number2 = getRandomNumber();
+    console.log(`Question: ${number1} ${number2}`);
+    const youAnswer = readlineSync.question("You answer: ");
+    let gcd;
     while (number1 !== number2) {
-  if (number1 > number2) {
-    number1 = number1 - number2;
-  }
-  else {
-    number2 = number2 - number1;
-  }
-}
-gcd = number1;
-return gcd;
+      if (number1 > number2) {
+        number1 = number1 - number2;
+      } else {
+        number2 = number2 - number1;
+      }
     }
-        nod (number1, number2);
+    gcd = number1;
     if (youAnswer === `${gcd}`) {
-        console.log("Correct!");
-        return attemptCounter += 1;
+      console.log("Correct!");
+      return (attemptCounter += 1);
     } else {
-        console.log(`'${youAnswer}' is wrong answer ;(. Correct answer was ${gcd}.`);
-        console.log(`Let's try again, ${username}!`);
-        return;
+      console.log(
+        `'${youAnswer}' is wrong answer ;(. Correct answer was ${gcd}.`
+      );
+      console.log(`Let's try again, ${username}!`);
+      return;
     }
-}
+  }
 
-if (attemptCounter === 3) {
-    console.log(`Congratulations, ${ username }!`);
-    }
-}
+  if (attemptCounter === 3) {
+    console.log(`Congratulations, ${username}!`);
+  }
+};
 export default gameGcd;
