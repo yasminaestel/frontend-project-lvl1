12404ed7b75a147e4bfbd29f7e58ef3ca45bbf19
Svetlana Khodak <yasminaestel@gmail.com>
2022-02-20 16:52:35 +0200

    add code in index-progression

--- src/games/index-progression.js
+++ src/games/index-progression.js
@@ -15,7 +15,7 @@ const gameProgression = () => {
     let difference = randomDiff();
     let arrayLength = randomLength();
     let lineMassive = [];
-    line = lineMassive.push(firstIndex)
+    let line = lineMassive.push(firstIndex)
     for (let index = 1; index < arrayLength; index += 1) {
     let intermediateVariable = lineMassive[index-1] + difference;
     lineMassive = lineMassive.push(intermediateVariable);
