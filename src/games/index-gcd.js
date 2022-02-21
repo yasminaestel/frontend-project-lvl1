07746dae384b432955f493c6a8cab69d7d607561
yasminaestel <98806463+yasminaestel@users.noreply.github.com>
2022-02-21 15:12:05 +0200

    Update index-gcd.js

--- src/games/index-gcd.js
+++ src/games/index-gcd.js
@@ -19,7 +19,11 @@ const gameGcd = () => {
     while (number1 !== number2) {
       if (number1 > number2) {
         number1 = number1 - number2;
-      } else {
+      } 
+      if (number1 === 0 || number2 === 0) {
+        number1 = 0;
+        number2 = number1; }
+          else {
         number2 = number2 - number1;
       }
     }
