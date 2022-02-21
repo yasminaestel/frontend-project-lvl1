9dab9b3d964020b3aba0ea62d72d7415a526c977
yasminaestel <98806463+yasminaestel@users.noreply.github.com>
2022-02-21 12:15:59 +0200

    Update index-prime.js

--- src/games/index-prime.js
+++ src/games/index-prime.js
@@ -12,33 +12,36 @@ const gamePrime = () => {
     let number = getRandomNumber();
     console.log(`Question: ${number}`);
     let correctAnswer;
-   const isPrime = (number) =>{
- if (number < 2) {
-    return correctAnswer = "no";
-    }
-    if (number === 2) {
-    return correctAnswer = "yes"; 
-    }
-    for (let i = 2; i <= number / 2; i += 1) {
+    const isPrime = (number) => {
+      if (number < 2) {
+        return (correctAnswer = "no");
+      }
+      if (number === 2) {
+        return (correctAnswer = "yes");
+      }
+      for (let i = 2; i <= number / 2; i += 1) {
         if (number % i === 0) {
-           return correctAnswer = "no";
+          return (correctAnswer = "no");
         }
+      }
+      return (correctAnswer = "yes");
+    };
+    isPrime(number);
+    const yourAnswer = readlineSync.question("You answer: ");
+    if (yourAnswer === correctAnswer) {
+      console.log("Correct!");
+      attemptCounter += 1;
+    } else {
+      console.log(
+        `${yourAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.`
+      );
+      console.log(`Let's try again, ${username}!`);
+      return;
     }
- return correctAnswer = "yes";}
-isPrime(number);
- const yourAnswer = readlineSync.question("You answer: ");
-  if (yourAnswer === correctAnswer) {
-    console.log("Correct!");
-    attemptCounter += 1;
-  } else {
-    console.log(`${yourAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
-    console.log(`Let's try again, ${username}!`);
-    return;
-  }
 
-  if (attemptCounter === 3) {
-    console.log(`Congratulations, ${username}!`);
+    if (attemptCounter === 3) {
+      console.log(`Congratulations, ${username}!`);
+    }
   }
-}
-}
+};
 export default gamePrime;
