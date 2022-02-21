192a433528fc0ae17270732964bfd32ef2f47c89
yasminaestel <98806463+yasminaestel@users.noreply.github.com>
2022-02-21 12:38:34 +0200

    Update cli.js

--- src/games/cli.js
+++ src/games/cli.js
@@ -2,7 +2,7 @@ import readlineSync from "readline-sync";
 console.log("Welcome to the Brain Games!");
 const username = () => {
 const userName = readlineSync.question("May I have your name? ");
-console.log(`Hello, ${username}!`);
+console.log(`Hello, ${userName}!`);
 }
 
 export default username;
