09f0e49394a6d2f2f33ae45f0ffec30b78dc2a7f
yasminaestel <98806463+yasminaestel@users.noreply.github.com>
2022-02-21 10:46:44 +0200

    Update index-prime.js

--- src/games/index-prime.js
+++ src/games/index-prime.js
@@ -7,37 +7,37 @@ const username = readlineSync.question("May I have your name? ");
 console.log(`Hello, ${username}!`);
 console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
 const gamePrime = () => {
-    let attemptCounter = 0;
+  let attemptCounter = 0;
   for (let i = 1; i <= 3; i += 1) {
     let number = getRandomNumber();
     console.log(`Question: ${number}`);
-    const youAnswer = readlineSync.question('You answer: ');
+    const youAnswer = readlineSync.question("You answer: ");
     let correctAnswer;
     if (number < 2) {
-        correctAnswer = "no";
+      correctAnswer = "no";
     }
 
     for (let i = 2; i <= number / 2; i += 1) {
-        if (number % i === 0) {
-            correctAnswer = 'no';
-        }
+      if (number % i === 0) {
+        correctAnswer = "no";
+      }
     }
 
-    correctAnswer = 'yes';
-};
-if (youAnswer === correctAnswer) {
-    console.log('Correct!');
+    correctAnswer = "yes";
+  }
+  if (youAnswer === correctAnswer) {
+    console.log("Correct!");
     attemptCounter += 1;
   } else {
-    console.log(`'${youAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
+    console.log(
+      `'${youAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.`
+    );
     console.log(`Let's try again, ${username}!`);
     return;
   }
 
-
-if (attemptCounter === 3) {
-  console.log(`Congratulations, ${username}!`);
-}
+  if (attemptCounter === 3) {
+    console.log(`Congratulations, ${username}!`);
+  }
 };
 export default gamePrime;
-
