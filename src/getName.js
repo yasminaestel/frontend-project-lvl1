aafd11d3cc3909b1858508faa2fad244e1d278d1
Svetlana Khodak <yasminaestel@gmail.com>
2023-06-13 15:31:04 +0300

    delet files

--- src/getName.js
+++
@@ -1,9 +0,0 @@
-import readlineSync from 'readline-sync';
-
-const getName = () => {
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-  return userName;
-};
-
-export default getName;
