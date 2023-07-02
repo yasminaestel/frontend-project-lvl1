b71738630a92703d5dce85b72c5c953d23c427ce
Svetlana Khodak <yasminaestel@gmail.com>
2023-07-02 17:56:48 +0300

    change brain-prodression

--- src/games/index-progression.js
+++ src/games/index-progression.js
@@ -10,22 +10,15 @@ const generateProgression = (firstIndex, difference, arrayLength) => {
   return progression;
 };
 
-export const getRoundGameProgression = () => {
+const getRoundGameProgression = () => {
   const firstIndex = getRandom(0, 101);
   const difference = getRandom(1, 10);
-  const arrayLength = getRandom(4, 12);
+  const arrayLength = getRandom(5, 10);
   const progression = generateProgression(firstIndex, difference, arrayLength);
 
-  let hiddenNumberIndex = getRandom(1, progression.length - 1);
-  let correctAnswer;
-  if (hiddenNumberIndex < progression.length) {
-    correctAnswer = progression[hiddenNumberIndex];
-    progression.splice(hiddenNumberIndex, 1, '..');
-  } else {
-    hiddenNumberIndex -= 5;
-    correctAnswer = progression[hiddenNumberIndex];
-    progression.splice(hiddenNumberIndex, 1, '..');
-  }
+  const hiddenNumberIndex = getRandom(0, progression.length - 1);
+  const correctAnswer = progression[hiddenNumberIndex];
+  progression.splice(hiddenNumberIndex, 1, '..');
 
   const question = `${progression.join(' ')}`;
   return [question, correctAnswer.toString()];
